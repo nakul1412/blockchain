0c24491d74a45677880df8f700d664c524e2c61f
nakul patel <nakulpatel1488@gmail.com>
2021-04-25 21:55:53 +0530

    assignment 1 summit

---
+++ assignment 1/assignment1.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+const cr=require('crypto');
+const name = prompt('enter the string input ');
+let target=0x0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+let hash,i=1,nname;
+nname=name+String(i);
+hash=cr.createHash('sha256').update(nname).digest('hex');
+while((parseInt(hash,16))>=(parseInt(target,16)))
+{ i++;
+  nname=name+String(i); 
+ hash=cr.createHash('sha256').update(nname).digest('hex');
+}
+console.log(i);
